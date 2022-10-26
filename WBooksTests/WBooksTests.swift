c7a32415377a33f515a3a633c311e6dfa24b17eb
AniiMancuso <ana.mancuso@accenture.com>
2022-10-26 10:23:44 -0300

    Format changes

--- WBooksTests/WBooksTests.swift
+++ WBooksTests/WBooksTests.swift
@@ -29,9 +29,7 @@ final class WBooksTests: XCTestCase {
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
     
